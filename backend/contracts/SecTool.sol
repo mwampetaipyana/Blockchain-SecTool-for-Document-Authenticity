cf8f9298403c5998aadfafd5e028371206eb47be
mwampetaipyana <mwampetaipyana256@gmail.com>
2024-06-12 18:32:41 +0300

    edit and generate report module added

--- backend/contracts/SecTool.sol
+++ backend/contracts/SecTool.sol
@@ -19,6 +19,7 @@ contract SecTool {
     }
 
     struct Document {
+        string docID;
         string title;
         string description;
         string ipfs_hash;
@@ -43,7 +44,8 @@ contract SecTool {
     Transaction[] public transactionsArray;
 
     // Mappings
-    mapping(address => Document) mydocumentsMapping;
+    mapping(address => Document[]) mydocumentsMapping;
+    mapping(string => Document[]) id_docMapping;
 
     // Event to log new transactions
     event NewTransaction(
@@ -76,7 +78,7 @@ contract SecTool {
         address _proAddress,
         string memory _organization,
         string memory _location
-    ) public {
+    ) public adminOnly{
         Pro memory newpro = Pro({
             fname: _fname,
             lname: _lname,
@@ -90,15 +92,17 @@ contract SecTool {
 
     // Function to store a new document
     function storeDocument(
+        string memory _docID,
         string memory _title,
         string memory _description,
         string memory _ipfs_hash
-    ) public {
+    ) public isPRO{
         require(bytes(_title).length > 0, "Title cannot be empty");
         require(bytes(_description).length > 0, "Description cannot be empty");
         require(bytes(_ipfs_hash).length > 0, "IPFS hash cannot be empty");
      
         Document memory newDocument = Document({
+            docID: _docID,
             title: _title,
             description: _description,
             ipfs_hash: _ipfs_hash,
@@ -107,7 +111,8 @@ contract SecTool {
         });
 
         documentsArray.push(newDocument);
-        mydocumentsMapping[msg.sender] = newDocument;
+        id_docMapping[_docID].push(newDocument);
+        mydocumentsMapping[msg.sender].push(newDocument);
         addTransaction("Store Document", msg.sender, "Success");
     }
 
@@ -132,25 +137,107 @@ contract SecTool {
     }
 
     // Function to edit a document's title, description, or IPFS hash by a Pro
-    function editDocument(
-        string memory _ipfs_hash,
-        string memory _newTitle,
-        string memory _newDescription,
-        string memory _newIpfsHash
-    ) public {
-        // Check if the document exists
-        require(
-            keccak256(bytes(mydocumentsMapping[msg.sender].ipfs_hash)) ==
-                keccak256(bytes(_ipfs_hash)),
-            "Document not found"
-        );
+  function editDocument(
+    string memory _docID,
+    string memory _newTitle,
+    string memory _newDescription,
+    string memory _newIpfsHash
+    ) public isPRO {
+    // Check if the document exists in the id_docMapping
+    require(
+        id_docMapping[_docID].length > 0,
+        "Document not found"
+    );
 
-        // Update the document's fields
-        mydocumentsMapping[msg.sender].title = _newTitle;
-        mydocumentsMapping[msg.sender].description = _newDescription;
-        mydocumentsMapping[msg.sender].ipfs_hash = _newIpfsHash;
+    // Find the index of the document in the documentsArray
+    for (uint256 i = 0; i < documentsArray.length; i++) {
+        if (keccak256(abi.encodePacked(documentsArray[i].docID)) 
+        == keccak256(abi.encodePacked(_docID))) {
+            // Replace the document in the documentsArray with the last document
+            documentsArray[i] = documentsArray[documentsArray.length - 1];
+            // Remove the last document from the array
+            documentsArray.pop();
+            break;
+        }
+    }
 
-        addTransaction("Edit Document", msg.sender, "Success");
+    // Remove the document from the uploader's array in mydocumentsMapping
+    uint256 uploaderIndex = 0;
+    for (; uploaderIndex < mydocumentsMapping[msg.sender].length; uploaderIndex++) {
+        if (keccak256(abi.encodePacked(mydocumentsMapping[msg.sender][uploaderIndex].docID)) 
+        == keccak256(abi.encodePacked(_docID))) {
+            break;
+        }
+    }
+    if (uploaderIndex < mydocumentsMapping[msg.sender].length) {
+        // Shift elements to fill the gap left by the removed document
+        for (uint256 j = uploaderIndex; j < mydocumentsMapping[msg.sender].length - 1; j++) {
+            mydocumentsMapping[msg.sender][j] = mydocumentsMapping[msg.sender][j + 1];
+        }
+        // Remove the last element (which is now the document we want to delete)
+        mydocumentsMapping[msg.sender].pop();
+    }
+
+     Document memory updatedDocument = Document({
+        docID: _docID,
+        title: _newTitle,
+        description: _newDescription,
+        ipfs_hash: _newIpfsHash,
+        timestamp: block.timestamp,
+        uploader: msg.sender
+    });
+
+    documentsArray.push(updatedDocument);
+
+     // Update the id_docMapping with the new document
+    id_docMapping[_docID][0] = updatedDocument;
+    mydocumentsMapping[msg.sender].push(updatedDocument);
+    addTransaction("Edit Document", msg.sender, "Success");
+    }
+
+
+    function deleteDoc(
+        string memory _docID) 
+        public isPRO{
+    
+    // Check if the document exists in the id_docMapping
+    require(
+        id_docMapping[_docID].length > 0,
+        "Document not found"
+    );
+
+    // Find the index of the document in the documentsArray
+    for (uint256 i = 0; i < documentsArray.length; i++) {
+        if (keccak256(abi.encodePacked(documentsArray[i].docID)) 
+        == keccak256(abi.encodePacked(_docID))) {
+            // Replace the document in the documentsArray with the last document
+            documentsArray[i] = documentsArray[documentsArray.length - 1];
+            // Remove the last document from the array
+            documentsArray.pop();
+            break;
+        }
+    }
+
+    // Remove the document from the uploader's array in mydocumentsMapping
+    uint256 uploaderIndex = 0;
+    for (; 
+    uploaderIndex < mydocumentsMapping[msg.sender].length; 
+    uploaderIndex++) {
+        if (keccak256(abi.encodePacked(mydocumentsMapping[msg.sender][uploaderIndex].docID))
+         == keccak256(abi.encodePacked(_docID))) {
+            break;
+        }
+    }
+    if (uploaderIndex < mydocumentsMapping[msg.sender].length) {
+        // Shift elements to fill the gap left by the removed document
+        for (uint256 j = uploaderIndex; 
+        j < mydocumentsMapping[msg.sender].length - 1;
+         j++) {
+            mydocumentsMapping[msg.sender][j] = mydocumentsMapping[msg.sender][j + 1];
+        }
+        // Remove the last element (which is now the document we want to delete)
+        mydocumentsMapping[msg.sender].pop();
+    }
     }
 
     // Function to view all PROs
@@ -162,11 +249,14 @@ contract SecTool {
     function viewDocumentsByPRO(address _proAddress)
         public
         view
-        returns (Document memory)
+        returns (Document[] memory)
     {
         return mydocumentsMapping[_proAddress];
     }
 
+    function getAllDocs() public view returns (Document[] memory){
+        return documentsArray;
+    }
     // Function to get transactions for a specific Pro
     function getTransactionsForPro(address _proAddress)
         public
@@ -191,5 +281,21 @@ contract SecTool {
         }
 
         return proTransactions;
+    }
+
+    modifier adminOnly() {
+        require(msg.sender == admin, "SecTool Admin account is required");
+        _;
+    }
+
+     modifier isPRO() {
+        bool isuser = false;
+        for (uint256 i = 0; i < prosArray.length; i += 1) {
+            if (msg.sender == prosArray[i].proAddress) {
+                isuser = true;
+            }
+        }
+        require(isuser, "only identity owner is allowed to perform this Action");
+        _;
     }
 }
